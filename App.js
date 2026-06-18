c4aaac9566d7ae1842e6cbf906bc24260d90121f
dt6368864-a11y <dt6368864@gmail.com>
2026-06-18 16:25:33 -0500

    hotfix: agregado banner de mantenimiento de emergencia por qa

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hola Mundo</Text>
+      <Text style={styles.texto}>⚠️ Modo Pruebas de Mantenimiento Activo</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' },
-  text: { fontSize: 24, fontWeight: 'bold' }
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f8d7da' },
+  texto: { color: '#721c24', fontWeight: 'bold', fontSize: 16 }
 });
